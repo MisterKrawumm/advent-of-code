f46e16a3a7e92591a52a7b61c710e07ae4ceb1a6
Fritz Gruber <fritzgruber243@gmail.com>
2022-12-04 17:44:28 +0100

    add day 01 / 1

---
+++ aoc_01/aoc_01/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string[] input = File.ReadAllLines("C:\\Users\\fritz\\Desktop\\01_Fritz.txt");
+
+
+
+List<long> calories= new List<long>();
+long summe=0;
+foreach (string line in input)
+{
+    if (line.Length == 0)
+    {
+        calories.Add(summe);
+        summe=0;
+    }
+    else
+    {
+        summe += long.Parse(line);
+    }
+}
+
+
+Console.WriteLine(calories.Max());
